57cf28dcb0494ccb56c9b26414d397e3200c3fcc
vshevchyk <volodymyr.shevchyk@indeema.com>
2020-02-08 15:27:23 +0200

    Implemented editing for platform cleanup scripts

--- api/platform/controllers/platform.js
+++ api/platform/controllers/platform.js
@@ -30,11 +30,40 @@ async getPlatform(ctx) {
     });
 },
 
+async getPlatformCleanup(ctx) {
+  const entity = ctx.params;
+  const path = require('path');
+  const directoryPath = path.resolve() +  `/scripts/platforms_cleanup/${entity.name}`;
+
+  return new Promise((rs, rj) => {
+    fs.readFile(directoryPath, (err, data) => {
+      if (err) rs({"status":"bad", "data":err});
+
+      rs({"status":"ok", "data": data.toString()});
+    });
+  });
+},
+
 async writePlatform(ctx) {
   console.log(ctx.request.body);
   const entity = ctx.params;
   const path = require('path');
   const directoryPath = path.resolve() +  `/scripts/platforms/${entity.name}`;
+
+  return new Promise((rs, rj) => {
+    fs.writeFile(directoryPath, ctx.request.body, (err) => {
+      if (err) rs({"status":"ok", "data":err});
+
+      rs({"status":"ok", "data":entity.name});
+    }); 
+  }); 
+},
+
+async writePlatformCleanup(ctx) {
+  console.log(ctx.request.body);
+  const entity = ctx.params;
+  const path = require('path');
+  const directoryPath = path.resolve() +  `/scripts/platforms_cleanup/${entity.name}`;
 
   return new Promise((rs, rj) => {
     fs.writeFile(directoryPath, ctx.request.body, (err) => {
@@ -59,9 +88,37 @@ async deletePlatform(ctx) {
   });
 },
 
+async deletePlatformCleanup(ctx) {
+  const entity = ctx.params;
+  const path = require('path');
+  const directoryPath = path.resolve() +  `/scripts/platforms_cleanup/${entity.name}`;
+
+  return new Promise((rs, rj) => {
+    fs.unlink(directoryPath, (err) => {
+      if (err) rs({"status":"bad", "data":err});
+
+      rs({"status":"ok", "data":"The file was succesfully saved!"});
+    });
+  });
+},
+
 async getListOfPlatforms(ctx) {
   const path = require('path');
   const directoryPath =  path.resolve() + '/scripts/platforms/';
+  
+  return new Promise((rs, rj) => {
+    fs.readdir(directoryPath, function (err, files) {
+      files = files.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item));
+      if (err) rs(console.log({"status":"bad", "data": err}));
+      
+      rs({"status":"ok", "data":files});
+    });
+  });
+},
+
+async getListOfPlatformsCleanup(ctx) {
+  const path = require('path');
+  const directoryPath =  path.resolve() + '/scripts/platforms_cleanup/';
   
   return new Promise((rs, rj) => {
     fs.readdir(directoryPath, function (err, files) {
