035f459c6031348c5e9a2ac9e83be707d9818e2b
vshevchyk <volodymyr.shevchyk@indeema.com>
2020-02-13 11:49:34 +0200

    Cleaning up the project. Finishind server setup/cleanup script calls

--- api/console/controllers/console.js
+++ api/console/controllers/console.js
@@ -101,26 +101,48 @@ module.exports = {
       });
     }
 
-    var command = resourcesPath + 'platforms/${server.server_name} ';
+    var command = resourcesPath + `platforms/${server.platform.platform_name} `;
     const commandConnect = exec(command);
     commandConnect.stdout.on('data', async function(data){
       if(data !== ''){
         const consoleItem = await strapi.services.console.create({
           message: data,
           type: 'message',
-          app: APP_ID
+          server: server._id.toString()
         });
 
         //Set status project
-        await strapi.services.app.update({
-          id: APP_ID
+        await strapi.services.server.update({
+          id: server._id.toString()
         }, {
-          app_status: 'progress'
+          server_status: 'progress'
         });
 
         //Send message
         strapi.eventEmitter.emit('system::notify', {
-          topic: `/console/setup/${APP_ID}/message`,
+          topic: `/console/setup/${server._id.toString()}/message`,
+          data: consoleItem.message
+        });
+      }
+    });
+    commandConnect.stderr.on('data', async function(data){
+      if(data !== ''){
+        const consoleItem = await strapi.services.console.create({
+          message: data,
+          type: 'error',
+          server: server._id.toString()
+        });
+
+        //Set status project
+        await strapi.services.server.update({
+          id: server._id.toString()
+        }, {
+          server_status: 'failed'
+        });
+
+        //Send message
+        strapi.eventEmitter.emit('system::notify', {
+          topic: `/console/setup/${server._id.toString()}/error`,
           data: consoleItem.message
         });
       }
