76fb1eb34b2e0bcbc0f6e6fb42d62ece0490c1bf
vshevchyk <volodymyr.shevchyk@indeema.com>
2020-03-01 14:35:39 +0200

    Implemented new way of maintenance settings

--- api/maintenance/controllers/maintenance.js
+++ api/maintenance/controllers/maintenance.js
@@ -1,72 +1,8 @@
 'use strict';
 
-const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
-const fs = require('fs');
-const _ = require('lodash');
-const path = require('path');
-const resourcesPath = path.resolve() + "/public/uploads/maintenance/" 
-
 /**
- * default bookshelf controller
- *
+ * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
  */
-module.exports = {
-  /**
-   * Get ci template 
-   *
-   * @param ctx
-   * @returns {Promise<void>}
-   */
-async getMaintenance(ctx) {
-    const entity = ctx.params;
-    const directoryPath = resourcesPath + `${entity.name}`;
 
-    return new Promise((rs, rj) => {
-      fs.readFile(directoryPath, (err, data) => {
-        if (err) rs({"status":"bad", "data":err});
-
-        rs({"status":"ok", "data": data.toString()});
-      });
-    });
-},
-
-async writeMaintenance(ctx) {
-  const entity = ctx.params;
-  const directoryPath = resourcesPath + `${entity.name}`;
-
-  return new Promise((rs, rj) => {
-    fs.writeFile(directoryPath, ctx.request.body.data, (err) => {
-      if (err) rs({"status":"ok", "data":err});
-
-      rs({"status":"ok", "data":entity.name});
-    }); 
-  }); 
-},
-
-async deleteMaintenance(ctx) {
-  const entity = ctx.params;
-  const directoryPath = resourcesPath + `${entity.name}`;
-
-  return new Promise((rs, rj) => {
-    fs.unlink(directoryPath, (err) => {
-      if (err) rs({"status":"bad", "data":err});
-
-      rs({"status":"ok", "data":"The file was succesfully saved!"});
-    });
-  });
-},
-
-async getListOfMaintenance(ctx) {
-  const directoryPath =  resourcesPath;
-  
-  return new Promise((rs, rj) => {
-    fs.readdir(directoryPath, function (err, files) {
-      files = files.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item));
-      if (err) rs(console.log({"status":"bad", "data": err}));
-      
-      rs({"status":"ok", "data":files});
-    });
-  });
-}
-
-};
+module.exports = {};
--- api/maintenance/models/maintenance.js
+++ api/maintenance/models/maintenance.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /**
- * Lifecycle callbacks for the `project` model.
+ * Lifecycle callbacks for the `maintenance` model.
  */
 
 module.exports = {
