30c1b561ca742e685ef93fa88a6d5320d5fe8b1e
vshevchyk <volodymyr.shevchyk@indeema.com>
2020-03-04 20:46:53 +0200

    Implemented correct console logging for apps

--- api/console/services/console.js
+++ api/console/services/console.js
@@ -6,24 +6,114 @@
  */
 const exec = require('child_process').exec;
 var express = require('express');
+var server = express();
 var app = express();
 
 app.listen(3000, function () {
-  updateMessage();
-  setInterval(updateMessage, 1000);
+  messages();
+  setInterval(messages, 1000);
 });
 
-var messages = [{key:"",value:""}];
+var gServerMessages = [{key:"",value:""}];
 var gServer = null;
-var gStatus = null;
+var gServerStatus = null;
+
+var gAppMessages = [{key:"",value:""}];
+var gApp = null;
+var gAppStatus = null;
 
 function msleep(n) {
   Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
 }
 
-async function updateMessage() {
-  while (messages.length > 0) {
-    let object = messages.shift(); 
+async function messages() {
+  updateAppMessage();
+  updateServerMessage();
+}
+
+async function updateAppMessage() {
+  while (gAppMessages.length > 0) {
+    let object = gAppMessages.shift(); 
+    console.log(object);
+    if (object.key === 'data') {
+      const consoleItem = await strapi.services.console.create({
+        message: object.value,
+        type: 'message',
+        app: gApp.id
+        });
+
+        //Set status app
+        strapi.services.app.update({
+          id: gApp.id
+        }, {
+          app_status: gAppStatus.progress.status
+        });
+
+        //Send message
+        strapi.eventEmitter.emit('system::notify', {
+        topic: `/console/setup/${gApp.id}/message`,
+        data: consoleItem.message
+        });
+    } else if (object.key === 'close') {
+      let code = object.value
+
+      const consoleItem = await strapi.services.console.create({
+        message: `Child process exited with code ${code}`,
+        type: 'end',
+        app: gApp.id
+      });
+      //Send message
+      strapi.eventEmitter.emit('system::notify', {
+        topic: `/console/setup/${gApp.id}/end`,
+        data: consoleItem.message
+      });
+
+      if(code !== 0){
+         strapi.services.console.create({
+          message: gAppStatus.bad.info,
+          type: 'build_error',
+          app: gApp.id
+        });
+
+        //Set status app
+        strapi.services.app.update({
+            id: gApp.id
+        }, {
+            app_status: gAppStatus.bad.status
+        });
+
+        //Send message
+        strapi.eventEmitter.emit('system::notify', {
+          topic: `/console/setup/${gApp.id}/build_error`,
+          data: gAppStatus.bad.info
+        });
+      } else {
+        const consoleItem = await strapi.services.console.create({
+          message: gAppStatus.ok.info,
+          type: 'end',
+          app: gApp.id
+        });
+
+        //Set status app
+        strapi.services.app.update({
+          id: gApp.id
+        }, {
+            app_status: gAppStatus.ok.status
+        });
+
+        //Send message
+        strapi.eventEmitter.emit('system::notify', {
+          topic: `/console/setup/${gApp.id}/build_success`,
+          data: consoleItem.message
+        });
+      }
+    }
+  }
+}
+
+async function updateServerMessage() {
+  while (gServerMessages.length > 0) {
+    let object = gServerMessages.shift(); 
     console.log(object);
     if (object.key === 'data') {
       const consoleItem = await strapi.services.console.create({
@@ -35,7 +125,7 @@ async function updateMessage() {
       await strapi.services.server.update({
         id: gServer.id
       }, {
-        server_status: gStatus.progress.status
+        server_status: gServerStatus.progress.status
       });
       //Send message
       await strapi.eventEmitter.emit('system::notify', {
@@ -58,7 +148,7 @@ async function updateMessage() {
 
       if(code !== 0){
         await strapi.services.console.create({
-          message: gStatus.bad.info,
+          message: gServerStatus.bad.info,
           type: 'build_error',
           server: server.id
         });
@@ -67,17 +157,17 @@ async function updateMessage() {
         await strapi.services.server.update({
           id: server.id
         }, {
-          server_status: gStatus.bad.status
+          server_status: gServerStatus.bad.status
         });
 
         //Send message
         await strapi.eventEmitter.emit('system::notify', {
           topic: `/console/setup/${server.id}/build_error`,
-          data: gStatus.bad.info
+          data: gServerStatus.bad.info
         });
       } else {
         const consoleItem = await strapi.services.console.create({
-          message: gStatus.ok.info,
+          message: gServerStatus.ok.info,
           type: 'end',
           server: gServer.id
         });
@@ -86,7 +176,7 @@ async function updateMessage() {
         await strapi.services.server.update({
           id: gServer.id
         }, {
-          server_status: gStatus.ok.status
+          server_status: gServerStatus.ok.status
         });
 
         //Send message
@@ -99,141 +189,66 @@ async function updateMessage() {
   } 
 }
 module.exports = {
-    async updateMessage(server, status) {
-
-      while (messages.length > 0) {
-        const consoleItem = await strapi.services.console.create({
-          message: messages.pop(),
-          type: 'message',
-          server: server.id
-        });
-        //Set status server
-        strapi.services.server.update({
-          id: server.id
-        }, {
-          server_status: status.progress.status
-        });
-        //Send message
-        strapi.eventEmitter.emit('system::notify', {
-          topic: `/console/setup/${server.id}/message`,
-          data: consoleItem.message
-        });   
-      } 
-    },
-
-    async runServerScript(server, command, status) {
-        return new Promise((rs, rj) => {
-          gServer = server;
-          gStatus = status;    
-          
-          const commandConnect = exec(command);
-          commandConnect.stdout.on('data', async function (data) {
-            console.log("new data" + messages.length);
-            for (let tmpData of JSON.stringify(data).split(`\\r\\n`)) {
-              for (let tmp of tmpData.split(`\\r`)) {
-                tmp = tmp.replace('\"', '');
-                tmp = tmp.replace('\"', '');
-                tmp = tmp.replace('\\"', '');
-                tmp = tmp.replace('\\t', '');
-                tmp = tmp.replace('\n', '');
-                tmp = tmp.replace('\\', '');
-                tmp = tmp.replace('"', '');
-                tmp = tmp.replace('"', '');
-                // console.log(tmp);
-                if (tmp !== '' && tmp !== '\\r' && tmp !== '\\n' && tmp !== ' ') {    
-                  messages.push({key:"data", value:tmp});        
-                }
-              }
+  async runServerScript(server, command, status) {
+    return new Promise((rs, rj) => {
+      gServer = server;
+      gServerStatus = status;    
+      
+      const commandConnect = exec(command);
+      commandConnect.stdout.on('data', async function (data) {
+        for (let tmpData of JSON.stringify(data).split(`\\r\\n`)) {
+          for (let tmp of tmpData.split(`\\r`)) {
+            tmp = tmp.replace('\"', '');
+            tmp = tmp.replace('\"', '');
+            tmp = tmp.replace('\\"', '');
+            tmp = tmp.replace('\\t', '');
+            tmp = tmp.replace('\n', '');
+            tmp = tmp.replace('\\', '');
+            tmp = tmp.replace('"', '');
+            tmp = tmp.replace('"', '');
+            // console.log(tmp);
+            if (tmp !== '' && tmp !== '\\r' && tmp !== '\\n' && tmp !== ' ') {    
+              gServerMessages.push({key:"data", value:tmp});        
             }
-            // strapi.services.console.updateMessage(server, status);
-          });
-          commandConnect.on('close', async (code) => {
-            messages.push({key:"close", value:code});
-            console.log("close" + messages.length);
-          });
-          rs({status:"ok", data:"Good"});
-        });
-      },
-
-      async runAppScript(app, command, status) {
-        return new Promise((rs, rj) => {
-          const commandConnect = exec(command);
-                                      commandConnect.stdout.on('data', async function(data) {
-          if(data !== ''){
-              const consoleItem = await strapi.services.console.create({
-              message: `data`,
-              type: 'message',
-              app: app.id
-              });
-      
-              //Set status app
-              strapi.services.app.update({
-                id: app.id
-              }, {
-                app_status: status.progress.status
-              });
-      
-              //Send message
-              strapi.eventEmitter.emit('system::notify', {
-              topic: `/console/setup/${app.id}/message`,
-              data: consoleItem.message
-              });
           }
-          });
+        }
+      });
+      commandConnect.on('close', async (code) => {
+        gServerMessages.push({key:"close", value:code});
+      });
+      rs({status:"ok", data:"Good"});
+    });
+  },
 
-          commandConnect.on('close', async (code) => {
-              const consoleItem = await strapi.services.console.create({
-                message: `Child process exited with code ${code}`,
-                type: 'end',
-                app: app.id
-              });
-              //Send message
-              strapi.eventEmitter.emit('system::notify', {
-                topic: `/console/setup/${app.id}/end`,
-                data: consoleItem.message
-              });
-      
-              if(code !== 0){
-                 strapi.services.console.create({
-                  message: status.bad.info,
-                  type: 'build_error',
-                  app: app.id
-                });
-      
-                //Set status app
-                strapi.services.app.update({
-                    id: app.id
-                }, {
-                    app_status: status.bad.status
-                });
-      
-                //Send message
-                strapi.eventEmitter.emit('system::notify', {
-                  topic: `/console/setup/${app.id}/build_error`,
-                  data: status.bad.info
-                });
-              } else {
-                const consoleItem = await strapi.services.console.create({
-                  message: status.ok.info,
-                  type: 'end',
-                  app: app.id
-                });
-    
-                //Set status app
-                strapi.services.app.update({
-                  id: app.id
-                }, {
-                    app_status: status.ok.status
-                });
-    
-                //Send message
-                strapi.eventEmitter.emit('system::notify', {
-                  topic: `/console/setup/${app.id}/build_success`,
-                  data: consoleItem.message
-                });
+  async runAppScript(app, command, status) {
+    return new Promise((rs, rj) => {
+      gApp = app;
+      gAppStatus = status;
+
+      const commandConnect = exec(command);
+      commandConnect.stdout.on('data', async function(data) {
+        for (let tmpData of JSON.stringify(data).split(`\\r\\n`)) {
+          for (let tmp of tmpData.split(`\\r`)) {
+            console.log(tmp);
+            tmp = tmp.replace('\"', '');
+            tmp = tmp.replace('\"', '');
+            tmp = tmp.replace('\\"', '');
+            tmp = tmp.replace('\\t', '');
+            tmp = tmp.replace('\n', '');
+            tmp = tmp.replace('\\', '');
+            tmp = tmp.replace('"', '');
+            tmp = tmp.replace('"', '');
+            // console.log(tmp);
+            if (tmp !== '' && tmp !== '\\r' && tmp !== '\\n' && tmp !== ' ') {    
+              gAppMessages.push({key:"data", value:tmp});        
             }
-            });
-          rs({status:"ok", data:"Good"});
-        });
-      },
+          }
+        }
+      });
+      commandConnect.on('close', async (code) => {
+        gAppMessages.push({key:"close", value:code});
+      });
+      rs({status:"ok", data:"Good"});
+    });
+  },
 };
